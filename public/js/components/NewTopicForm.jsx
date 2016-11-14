763082c3a4d7f79d304625c9297e0bf915b8f1ab
Nick Ang <nickangtc@gmail.com>
2016-11-14 20:24:32 +0800

    Added dynamic autocomplete functionality to search bar on front page, using <input list> and <datalist>

--- public/js/components/NewTopicForm.jsx
+++ public/js/components/NewTopicForm.jsx
@@ -34,11 +34,32 @@ const NewTopicForm = React.createClass({
       }
     });
   },
+  findSimilarTopics: function (searchTerm) {
+    var result = [];
+    var term = searchTerm.toLowerCase();
+    if (this.state.topics) {
+      for (var i = 0; i < this.state.topics.length; i++) {
+        var topic = this.state.topics[i].title.toLowerCase();
+        if ( topic.includes(term) ) {
+          result.push(this.state.topics[i].title);
+        }
+      }
+      var similarTopics = result.map(function (topic, index) {
+        return (
+          <option value={topic} key={index} />
+        );
+      });
+      return similarTopics;
+    } else { return '' }
+  },
   render: function () {
     return (
       <form>
+        <datalist id="similar-topics">
+          {this.findSimilarTopics(this.state.topic)}
+        </datalist>
         <div className="form-group">
-          <input id="topic-input" type="text" data-toggle="dropdown" onChange={this.handleTopicInput} placeholder="how will AI co-exist with humanity 20 years from now?" className="form-control restrict-width"/>
+          <input id="topic-input" list="similar-topics" type="text" data-toggle="dropdown" onChange={this.handleTopicInput} placeholder="how will AI co-exist with humanity 20 years from now?" className="form-control restrict-width"/>
         </div>
         <button onClick={this.handleCreate} className="btn btn-success">make room</button>
       </form>
--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,4 @@ $(document).ready(function () {
   });
 
   $('.dropdown-toggle').dropdown()
-
-
-
 })
